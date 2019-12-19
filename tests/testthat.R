230aeca9a3a5ba9c68d8cbb156472a735ac6195e
N-DucharmeBarth <nicholasd@spc.int>
2019-12-19 12:33:53 +1100

    skip tests on travis, test locally

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,4 @@
 library(testthat)
 library(ndd.vast.utils)
 
-# set path to where the data is
-	testdata.dir = system.file("testdata", package="ndd.vast.utils")
-
 test_check("ndd.vast.utils")
--- tests/testthat/test-fse.R
+++ tests/testthat/test-fse.R
@@ -5,11 +5,14 @@ test_that("fine scale, strata, and enviro is working",
   			data(Data_Geostat)
   			data(enviro)
   			data(skj.alt2019.shp)
+  			
+  		# set path to where the data is
+			testdata.dir = system.file("testdata", package="ndd.vast.utils")
 
   		# Bring in solution
-  			load( file.path(testdata.dir,"fse.mgc.RData") )
-  			load( file.path(testdata.dir,"fse.par.RData") )
-  			load( file.path(testdata.dir,"fse.index_cyl.RData") )
+  			load( file.path(testdata.dir,"/fse.mgc.RData") )
+  			load( file.path(testdata.dir,"/fse.par.RData") )
+  			load( file.path(testdata.dir,"/fse.index_cyl.RData") )
 
   		# fit model
   			vast_output = fit.vast(Data_Geostat,RunDir=getwd(),SaveDir=getwd(),Q_ik = NULL,vf.re = FALSE,FieldConfig=c(Omega1 = 1, Epsilon1 = 1, Omega2 = 1, Epsilon2 = 1),RhoConfig=c(Beta1 = 0, Beta2 = 0, Epsilon1 = 0, Epsilon2 = 0),ObsModel_ez = c(1,3),fine_scale=TRUE,input.grid.res=1,knot_method = "grid",n_x=100,Version="VAST_v8_3_0",Method="Mesh",strata.sp=skj.alt2019.shp,enviro=enviro)
