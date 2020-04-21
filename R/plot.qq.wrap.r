cb84252cf14f5289da447f7ce2e2bffc88061851
N-DucharmeBarth <nicholasd@spc.int>
2020-04-21 11:28:05 +1100

    add gamma dist to plot.qq.wrap.r but 'stop' message remains

--- R/plot.qq.wrap.r
+++ R/plot.qq.wrap.r
@@ -14,9 +14,9 @@
 
 plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
 {	
-	if(error.structure != "dln")
+	if(!(error.structure %in% c("dln","dg")))
 	{
-		stop("Not defined for models not using a delta-lognormal error structure")
+		stop("Only defined for models not using a delta-lognormal or delta-gamma error structure")
 	}
 
 	######################################################
@@ -37,8 +37,9 @@ plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
 		    return(bino.res)
 		}
 
-		calc.qq.lnorm = function (vast.output, nsim=1000)
+		calc.qq.pos = function (vast.output, nsim=1000,error.structure)
 		# modified from Laura Tremblay-Boyer code used in 2018 alb assessment
+		# Jim Thorson's FishStatsUtils::plot_quantile_diagnostic
 		{
 		    TmbData = vast.output$TmbData
 		    Report = vast.output$Report
@@ -50,13 +51,26 @@ plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
 		    y = array(NA, dim = c(length(Which), nsim))
 		    var_y = rep(NA, length(Which))
 		    ## get prediction of indiviuals caught (rate * area swept)
-		   		pred_y = Report$R2_i[Which] * TmbData$a_i[Which]
-		        mean_y = log(pred_y) - pow(Report$SigmaM[1], 2)/2 # corrected mean is log_exp - 0.5sigma^2
+		   		pred_y = exp(Report$P2_i[Which]) * TmbData$a_i[Which]
+		   		if(error.structure == "dln")
+		   		{
+			        mean_y = log(pred_y) - pow(Report$SigmaM[1,1],2)/2 # corrected mean is log_exp - 0.5sigma^2
+			        y = rlnorm(n=nsim*length(Which), meanlog=rep(mean_y, each=nsim), sdlog=Report$SigmaM[1,1])
+			        dim(y) = c(nsim, length(Which)) ## draws in rows, observations in columns
+			        var_y = apply(y, 2, var) ## estimate of variance
+			        stdres = (log(TmbData$b_i[Which]) - mean_y)
+		   		} else if (error.structure == "dg"){
+		   			stop("I lied, I haven't implemented this yet. Try again with error.structure = 'dln'.")
+		   			gamma.scale = pow(Report$SigmaM[1,1],2) * pred_y;
+		   			mean_y = gamma.scale*(1/pow(sigmaM[i_e,1],2))
+         			y = rgamma(n=nsim*length(Which), shape=rep(1/pow(sigmaM[i_e,1],2), each=nsim), scale=gamma.scale)
+         			dim(y) = c(nsim, length(Which)) ## draws in rows, observations in columns
+			        var_y = apply(y, 2, var) ## estimate of variance
+			        stdres = TmbData$b_i[Which] - mean_y
+		   		} else {
+		   			stop("How did you sneak an other error structure into this function?? Try again with error.structure = 'dln' or 'dg'.")
+		   		}
 
-		        y = rlnorm(n=nsim*length(Which), meanlog=rep(mean_y, each=nsim), sdlog=Report$SigmaM[1])
-		        dim(y) = c(nsim, length(Which)) ## draws in rows, observations in columns
-		        var_y = apply(y, 2, var) ## estimate of variance
-		        stdres = (log(TmbData$b_i[Which]) - mean_y)
 		        pos.res = list(var_y = var_y, pred_y = pred_y, stdres=stdres,nsim=nsim)
 		        pos.res$which.pos = Which
 
@@ -71,7 +85,7 @@ plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
     			par(mfrow=c(1,2), mar=c(4,5,1,1), las=1)
 	   			qq.bin = calc.qq.binom(vast.output)
 	    		ab = qqnorm(qq.bin, plot.it=FALSE)
-		    	qqnorm(qq.bin, main='Binomial response',col=scales::alpha('dodgerblue4',0.3),cex.axis=1.5,cex.lab=1.5)
+		    	qqnorm(qq.bin, main='Encounter-rate component',col=scales::alpha('dodgerblue4',0.3),cex.axis=1.5,cex.lab=1.5)
 		   		q2get = c(0.001,0.01,0.025,0.975,0.99,0.999)
 	            q2p = quantile(ab$x, q2get)
 	            abline(v=q2p, col='azure3')
@@ -79,9 +93,9 @@ plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
 	            text(q2p, pu[4]-0.15, q2get, offset=0.3, col='azure3', srt=90, pos=2, xpd=NA, cex=0.8)
 		    	qqline(qq.bin, col='grey50')
 		    	rm(list=c("ab","q2get","q2p","pu"))
-				pos.res = calc.qq.lnorm(vast.output,nsims)
+				pos.res = calc.qq.pos(vast.output,nsims,error.structure)
 				ab = qqnorm(pos.res$stdres, plot.it=FALSE)
-				plot(ab$x, ab$y, type="n", col=scales::alpha('dodgerblue4',0.3), xlab='Theoretical quantiles', ylab='Sample quantiles', main='Lognormal response',las=1,cex.axis=1.5,cex.lab=1.5)
+				plot(ab$x, ab$y, type="n", col=scales::alpha('dodgerblue4',0.3), xlab='Theoretical quantiles', ylab='Sample quantiles', main='Positive catch-rate component',las=1,cex.axis=1.5,cex.lab=1.5)
 	   			qqline(pos.res$stdres, col='grey50')
 	            q2get = c(0.001,0.01,0.025,0.975,0.99,0.999)
 	            q2p = quantile(ab$x, q2get)
@@ -95,7 +109,7 @@ plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
     			par(mfrow=c(1,2), mar=c(4,5,1,1), las=1)
 	   			qq.bin = calc.qq.binom(vast.output)
 	    		ab = qqnorm(qq.bin, plot.it=FALSE)
-		    	qqnorm(qq.bin, main='Binomial response',col=scales::alpha('dodgerblue4',0.3),cex.axis=1.5,cex.lab=1.5)
+		    	qqnorm(qq.bin, main='Encounter-rate component',col=scales::alpha('dodgerblue4',0.3),cex.axis=1.5,cex.lab=1.5)
 		   		q2get = c(0.001,0.01,0.025,0.975,0.99,0.999)
 	            q2p = quantile(ab$x, q2get)
 	            abline(v=q2p, col='azure3')
@@ -103,9 +117,9 @@ plot.qq.wrap = function(vast.output,error.structure="dln",nsims=1000,save.dir)
 	            text(q2p, pu[4]-0.15, q2get, offset=0.3, col='azure3', srt=90, pos=2, xpd=NA, cex=0.8)
 		    	qqline(qq.bin, col='grey50')
 		    	rm(list=c("ab","q2get","q2p","pu"))
-				pos.res = calc.qq.lnorm(vast.output,nsims)
+				pos.res = calc.qq.pos(vast.output,nsims,error.structure)
 				ab = qqnorm(pos.res$stdres, plot.it=FALSE)
-				plot(ab$x, ab$y, type="n", col=scales::alpha('dodgerblue4',0.3), xlab='Theoretical quantiles', ylab='Sample quantiles', main='Lognormal response',las=1,cex.axis=1.5,cex.lab=1.5)
+				plot(ab$x, ab$y, type="n", col=scales::alpha('dodgerblue4',0.3), xlab='Theoretical quantiles', ylab='Sample quantiles', main='Positive catch-rate component',las=1,cex.axis=1.5,cex.lab=1.5)
 	   			qqline(pos.res$stdres, col='grey50')
 	            q2get = c(0.001,0.01,0.025,0.975,0.99,0.999)
 	            q2p = quantile(ab$x, q2get)
